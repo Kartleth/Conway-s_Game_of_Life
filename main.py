d1c905278e0225f375d7ee739816c79d14c068ac
Kartleth <Karlaarlethlerma@gmail.com>
2023-05-02 19:13:24 -0700

    Agregar paleta de colores

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import time
 import pygame
 import numpy as np
+
+COLOR_BG = (10,10,10) #black
+COLOR_GRID = (40,40,40) #grey
+COLOR_DIE_NEXT = (170,170,170)
+COLOR_ALIVE_NEXT = (250,250,250) #white
