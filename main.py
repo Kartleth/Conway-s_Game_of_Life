7d6e2a8f7260a57b98ddaa958c3adb204d15933d
Kartleth <Karlaarlethlerma@gmail.com>
2023-05-02 19:39:19 -0700

    Crear funcion main

--- main.py
+++ main.py
@@ -31,3 +31,41 @@ def update(screen, cells, size, with_progress=False):
         pygame.draw.rect(screen, color, (col * size, row * size, size - 1, size - 1))
     
     return update_cells
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800,600))
+
+    cells = np.zeros((60,80))
+    screen.fill(COLOR_GRID)
+    update(screen, cells, 10)
+
+    pygame.display.flip()
+    pygame.display.update()
+
+    running = False
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            elif event.tyoe == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    running = not running
+                    update(screen, cells, 10)
+                    pygame.display.update()
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+                cells[pos[1] // 10, pos[0] // 10] = 1
+                update(screen,cells,10)
+                pygame.display.update()
+        
+        screen.fill(COLOR_GRID)
+
+        if running:
+            cells = update(screen, cells, 10, with_progress=True)
+            pygame.display.update()
+        
+        time.sleep(0.001)
+        
