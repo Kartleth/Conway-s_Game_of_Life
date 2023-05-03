bf564fe1be2901555c715a1cb73bef8adefc63f9
Kartleth <Karlaarlethlerma@gmail.com>
2023-05-02 20:26:48 -0700

    Arreglos en la logica

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import numpy as np
 COLOR_BG = (10,10,10) #black
 COLOR_GRID = (40,40,40) #grey
 COLOR_DIE_NEXT = (170,170,170)
-COLOR_ALIVE_NEXT = (250,250,250) #white
+COLOR_ALIVE_NEXT = (255,255,255) #white
 
 def update(screen, cells, size, with_progress=False):
-    update_cells = np.empty((cells.shape[0], cells.shape[1]))
+    updated_cells = np.zeros((cells.shape[0], cells.shape[1]))
 
     for row, col in np.ndindex(cells.shape):
         alive = np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
@@ -18,19 +18,19 @@ def update(screen, cells, size, with_progress=False):
             if alive < 2 or alive > 3:
                 if with_progress:
                     color = COLOR_DIE_NEXT
-        elif 2 <= alive <= 3:
-            update_cells[row, col] = 1
-            if with_progress:
-                color = COLOR_ALIVE_NEXT
+            elif 2 <= alive <= 3:
+                updated_cells[row, col] = 1
+                if with_progress:
+                    color = COLOR_ALIVE_NEXT
         else: 
             if alive == 3:
-                update_cells[row, col] = 1
+                updated_cells[row, col] = 1
                 if with_progress:
                     color = COLOR_ALIVE_NEXT
         
         pygame.draw.rect(screen, color, (col * size, row * size, size - 1, size - 1))
     
-    return update_cells
+    return updated_cells
 
 def main():
     pygame.init()
@@ -50,7 +50,7 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-            elif event.tyoe == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     running = not running
                     update(screen, cells, 10)
@@ -68,4 +68,7 @@ def main():
             pygame.display.update()
         
         time.sleep(0.001)
-        
+
+
+if __name__ == '__main__':
+    main()
